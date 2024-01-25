9cdb72edd8472c8866e48199f83575a78d62c1f6
Demetrius Vissarion <demetrius.vissarion@gmail.com>
2024-01-25 10:34:15 +0000

    adjusted to use with gig_listing_website2

--- app.py
+++ app.py
@@ -1,14 +1,31 @@
 import os
-from flask import Flask, request
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/', methods=['GET'])
 def get_home():
     return "Hello, world!"
 
+@app.route('/endpoint', methods=['POST'])
+def post_data():
+    try:
+        data = request.json 
+        username = data.get('username')  
+        password = data.get('password')
+
+        # Perform any processing or store data as needed
+        # For now, just echoing back the received data
+        return jsonify({'message': f'Success! Received username: {username}, password: {password}'}), 200
+
+    except Exception as e:
+        return jsonify({'error': f'Error processing the request: {str(e)}'}), 400
+
+
 if __name__ == '__main__':
     app.run(
       debug=True,
-      host="0.0.0.0" # Listen for connections _to_ any server
+      host="0.0.0.0" 
     )
